ff0b1c24edecbb4fc3b888c6f3d5e6dc35a89da3
Jeremy <jeremy.jimenezvelasquez@unicolombo.edu.co>
2024-02-18 10:08:56 -0500

    primer ejercicio agregado

---
+++ Ejercicio1.java
@@ -0,0 +1,21 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicios_de_programacion;
+
+/**
+ *
+ * @author JEREMY
+ */
+public class Ejercicio1 {
+    public static void main(String sgrs[]){
+        
+        System.out.println("------------------------------");
+        System.out.println( "Fundamentos de Programación");
+        System.out.println("------------------------------");
+               
+    }
+    
+}
