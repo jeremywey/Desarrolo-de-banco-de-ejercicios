0b842ad29d24669a47c9056187bcea6debacdb9c
Jeremy <jeremy.jimenezvelasquez@unicolombo.edu.co>
2024-02-18 12:05:52 -0500

    quinto ejercicio agregado

---
+++ Ejercicio5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ejercicio5 {
+public static void main(String[]args){
+    Scanner scanner = new Scanner(System.in);
+double Diagonal,diagonal,area;
+System.out.println("Programa para calcular area de un rombo");
+System.out.println("");
+System.out.println("Ingrese los datos para calcular el area del rombo");
+System.out.println("---------------------------------------------------- ");
+System.out.println("Intrese la diagonal mayor");
+Diagonal = scanner.nextDouble();
+System.out.println("Intrese la diagonal menor");
+diagonal = scanner.nextDouble();
+  area = (Diagonal * diagonal) / 2;
+  System.out.println("El area del rombo es de: "+area+"cm²");
+
+}
+}
