940b2fc9f60859c033e198714ecded45e423b605
Jeremy <jeremy.jimenezvelasquez@unicolombo.edu.co>
2024-02-18 10:13:00 -0500

    segundo ejercicio agregado

---
+++ Ejercicio2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicios_de_programacion;
+
+/**
+ *
+ * @author JEREMY
+ */
+import java.util.Scanner;
+public class Ejercicio2 {
+    public static void main( String sgrs[]){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("------------------------------------");
+         System.out.println("---Ingrese cualquier comentario---");
+        System.out.println("------------------------------------");
+        String coment = scanner.nextLine();
+        System.out.println("");
+        System.out.println(coment);
+    }
+}
