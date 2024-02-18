3c222d3dd032abbaaad35670fc56e70d5b639b34
Jeremy <jeremy.jimenezvelasquez@unicolombo.edu.co>
2024-02-18 13:10:17 -0500

    noveno ejercicio agregado

---
+++ Ejercicio9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio9 {
+public static void main(String[] args) {
+    double pesos,monto, IVA = 0.16,e;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Programa que determina el monto del IVA sobre una cantidad dada");
+    System.out.println("----------------------------------------------------------------");
+    System.out.println("Ingrese una cantidad de pesos(COP) para determinar su IVA");
+     pesos = scanner.nextDouble();
+
+    monto = pesos * IVA;
+   System.out.println("El monto extra que va a pagar por el IVA es de "+monto);
+   System.out.println("precio final es de: "+(monto + pesos));
+   scanner.close();
+}
+}
