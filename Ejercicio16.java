9ea7f2d892e12ec3696f8045befb99ec139b70cf
Jeremy <jeremy.jimenezvelasquez@unicolombo.edu.co>
2024-02-20 12:49:36 -0500

    Decimosexto ejercicio agregado

---
+++ Ejercicio16.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        double nota;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Programa que determina si ganaste o perdiste un examen");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Las notas se evaluaran del 0 al 5");
+        System.out.println("Porfavor digite sus notas");
+        nota = scanner.nextDouble();
+        
+        if (nota > 5 || nota < 0) {
+            System.out.println("Dato invalido");
+        }else if(nota < 3){
+            System.out.println("Usted reprobo");
+            
+        }else{ 
+            System.out.println("Usted aprobo Felicidades");
+        }
+
+    }
+}
