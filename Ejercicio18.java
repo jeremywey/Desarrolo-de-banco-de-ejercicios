ad790bfa2bfffac8e790b3f75505f006cdb5b4eb
Jeremy <jeremy.jimenezvelasquez@unicolombo.edu.co>
2024-02-24 10:05:34 -0500

    Decimoctavo ejercicio agregado

--- Ejercicio18.java
+++ Ejercicio18.java
@@ -1,9 +1,9 @@
 public class Ejercicio18 {
     public static void main(String[] args) {
         System.out.println("El numero 125 al cuadrado es:");
-        double numero = 125;
-        numero = Math.pow(numero, 2);
+        int num = 125;
+        num =(int)Math.pow(num, 2);
         System.out.println("-----------------------------");
-    System.out.println(numero);
+    System.out.println(num);
     }
 }
