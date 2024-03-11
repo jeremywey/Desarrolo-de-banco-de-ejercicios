60404fa5afd073206d40e9093133ca271f8b951d
Jeremy <jeremy.jimenezvelasquez@unicolombo.edu.co>
2024-03-11 17:16:05 -0500

    Ejercicio 32 agregado

--- Ejercicio32.java
+++ Ejercicio32.java
@@ -5,17 +5,17 @@ public class Ejercicio32 {
         Scanner scanner = new Scanner(System.in);
         float cm,mm,base,altura,R;
 
-        System.out.println("programa que calcula el area de un triangulo en metros" );
+        System.out.println("programa que calcula el area de un rectangulo en metros" );
         System.out.println("-----------------------------------------------------------------");
-        System.out.println("Ingrese la base del triangulo primero");
+        System.out.println("Ingrese la base del rectangulo primero");
         base = scanner.nextFloat();
-        System.out.println(" Ahora ingrese la  altura del triangulo");
+        System.out.println(" Ahora ingrese la  altura del rectangulo");
         altura = scanner.nextFloat();
 
-        R = base *  altura / 2;
+        R = base *  altura ;
         cm = R * 100;
         mm = cm * 10;
-        System.out.println(" El area del treangulo es de: "+R+"m²");
+        System.out.println(" El area del rectangulo es de: "+R+"m²");
         System.out.println( "si este pasara a centimetros cuadrados: "+cm+"cm²");
         System.out.println(" si este pasara a metros cuadrados: "+mm+"mm²");
 
